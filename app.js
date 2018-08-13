08c21368df200bdb1a5efb00c80283ad360c9187
alextargonski <targonsky97@gmail.com>
2018-08-13 15:18:35 +0300

    add morgan HTTP request logger

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const app = express();
+const morgan  = require('morgan');
+
+const app     = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes   = require('./api/routes/orders');
 
+app.use(morgan('dev'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
