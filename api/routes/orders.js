2584d2e1fb9a57c4421c61eec1051f970eedb4da
alextargonski <targonsky97@gmail.com>
2018-08-16 16:09:44 +0300

    add populate

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,9 +5,9 @@ const mongoose = require("mongoose");
 const Order = require("../models/order");
 const Product = require("../models/product");
 
-// Handle incoming GET requests to /orders
 router.get("/", (req, res, next) => {
   Order.find()
+    .populate('product', 'name')
     .select("product quantity _id")
     .exec()
     .then(docs => {
@@ -73,6 +73,7 @@ router.post("/", (req, res, next) => {
 
 router.get("/:orderId", (req, res, next) => {
   Order.findById(req.params.orderId)
+    .populate('product')
     .exec()
     .then(order => {
       if (!order) {
