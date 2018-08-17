38445a1947849c4d83ae7d1b1dfd2ee7db64c852
alextargonski <targonsky97@gmail.com>
2018-08-17 12:25:53 +0300

    add image url to db

--- api/routes/products.js
+++ api/routes/products.js
@@ -32,7 +32,7 @@ const Product = require('../models/product');
 
 router.get("/", (req, res, next) => {
   Product.find()
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(docs => {
       const response = {
@@ -42,6 +42,7 @@ router.get("/", (req, res, next) => {
             name: doc.name,
             price: doc.price,
             _id: doc._id,
+            productImage: doc.productImage,
             request: {
               type: "GET",
               url: "http://localhost:3000/products/" + doc._id
@@ -63,7 +64,8 @@ router.post("/", upload.single('productImage'), (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
+    productImage: req.file.path
   });
   product
     .save()
@@ -93,7 +95,7 @@ router.post("/", upload.single('productImage'), (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(doc => {
       console.log("From database", doc);
