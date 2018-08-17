5cf6ced23831634b7c348362a2d04b905efb612e
alextargonski <targonsky97@gmail.com>
2018-08-17 15:30:49 +0300

    add user sign up

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app        = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes   = require('./api/routes/orders');
+const userRoutes    = require('./api/routes/user');
 
 mongoose.connect(
                   'mongodb://admin:' + process.env.MONGO_ATLAS_PW + '@cluster0-shard-00-00-vgwo3.mongodb.net:27017,cluster0-shard-00-01-vgwo3.mongodb.net:27017,cluster0-shard-00-02-vgwo3.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
