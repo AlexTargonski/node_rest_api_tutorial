8c3c52a6917e998cbfbbce1eec944308ffe84ecb
alextargonski <targonsky97@gmail.com>
2018-08-20 10:01:39 +0300

    add route protection

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express  = require('express');
 const router   = express.Router();
 const mongoose = require('mongoose');
 const multer   = require('multer');
+const checkAuth = require('../middleware/check-auth');
 
 const storage = multer.diskStorage({
   destination : function(req, file, cb) {
@@ -60,7 +61,7 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.post("/", upload.single('productImage'), (req, res, next) => {
+router.post("/", checkAuth, upload.single('productImage'), (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
@@ -92,7 +93,7 @@ router.post("/", upload.single('productImage'), (req, res, next) => {
     });
 });
 
-router.get("/:productId", (req, res, next) => {
+router.get("/:productId", checkAuth, (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
     .select('name price _id productImage')
@@ -113,7 +114,7 @@ router.get("/:productId", (req, res, next) => {
     });
 });
 
-router.patch("/:productId", (req, res, next) => {
+router.patch("/:productId", checkAuth, (req, res, next) => {
   const id = req.params.productId;
   const updateOps = {};
   for (const ops of req.body) {
